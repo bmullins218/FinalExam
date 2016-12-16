b72e11c3190292547ef9a1aa736a848434d2911b
billymullins <billymullins@billys-air.fios-router.home>
2016-12-15 23:40:45 -0500

    .

--- RocketDAL/src/test/java/rocketBase/Rate_Test.java
+++ RocketDAL/src/test/java/rocketBase/Rate_Test.java
@@ -1,30 +1,25 @@
 package rocketBase;
 
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import rocketDomain.RateDomainModel;
+import exceptions.RateException;
 
 public class Rate_Test {
 
 	
-	//TODO - RocketDAL rate_test
-	//		Check to see if a known credit score returns a known interest rate
-	
-	//TODO - RocketDAL rate_test
-	//		Check to see if a RateException is thrown if there are no rates for a given
-	//		credit score
+
 	@Test
-	public void test() {
-		
-		ArrayList<RateDomainModel> rates = RateDAL.getAllRates();
-		System.out.println ("Rates size: " + rates.size());
-		assert(rates.size() > 0);
-		
-		assert(1==1);
+	public void testRate() {
+		try{
+		assertTrue(RateDAL.getAllRates(555)==3.0);
+		assertTrue(RateDAL.getAllRates(888)==4.0);
+		}
+		catch(RateException e){
+			System.out.println("Credit Score Too Low");
+			
+		}
 	}
 
 }
